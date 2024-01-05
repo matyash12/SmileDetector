cebcf1befdfb712e708ad74ceb847f4410d83d32
matyash12 <hajekmatyas1@gmail.com>
2024-01-05 19:02:12 +0100

    Cleanup of useless things

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="title">Welcome</h1>
 </template>
